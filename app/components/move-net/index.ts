ce0637c353d651922efb4e049bd6d843e8a54511
Suhendri <suhendri_s@outlook.com>
2023-10-24 21:22:13 -0500

    feat(ember-tf): new component for move-net

---
+++ app/components/move-net/index.ts
@@ -0,0 +1,8 @@
+import Component from '@glimmer/component';
+import ApplicationInstance from '@ember/application/instance';
+
+export default class MoveNetComponent extends Component {
+  constructor(owner: ApplicationInstance, args: any) {
+    super(owner, args);
+  }
+}
